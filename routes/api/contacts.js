cc924b29876664d700c25bb66a7e85e8b99542ee
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2023-02-20 15:16:34 +0200

    add schemas and validate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,24 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 
+const { validateAddBody, validateUpdateBody } = require('../../middlewares/');
+
+const schemas = require('../../schemas/contacts');
+
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
 
 router.get('/:contactId', ctrl.getById);
 
-router.post('/', ctrl.add);
+router.post('/', validateAddBody(schemas.addSchema), ctrl.add);
 
 router.delete('/:contactId', ctrl.deleteById);
 
-router.put('/:contactId', ctrl.updateById);
+router.put(
+  '/:contactId',
+  validateUpdateBody(schemas.updateSchema),
+  ctrl.updateById
+);
 
 module.exports = router;
